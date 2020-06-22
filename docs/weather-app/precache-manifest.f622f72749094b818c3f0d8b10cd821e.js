053541bed3baeded9012bd966d1b20315b8351b8
sri-go <36702009+sri-go@users.noreply.github.com>
2020-06-22 17:36:41 -0400

    update weather app

---
+++ docs/weather-app/precache-manifest.f622f72749094b818c3f0d8b10cd821e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177f89d898ee475dde5712fdf34b5c55",
+    "url": "Assets/color/index.html"
+  },
+  {
+    "revision": "8202bc7d87bc197d8e90",
+    "url": "Assets/color/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "dd2dbd9ef2a22877d9b3",
+    "url": "Assets/color/static/css/main.ed4e2ce9.chunk.css"
+  },
+  {
+    "revision": "8202bc7d87bc197d8e90",
+    "url": "Assets/color/static/js/2.fc1e95a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "Assets/color/static/js/2.fc1e95a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2dbd9ef2a22877d9b3",
+    "url": "Assets/color/static/js/main.ab75b61b.chunk.js"
+  },
+  {
+    "revision": "3259f9b8248843d0b622",
+    "url": "Assets/color/static/js/runtime-main.5f0b2c53.js"
+  }
+]);
