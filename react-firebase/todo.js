08637b0557429d466c8d2c74027c4c6e627f150a
sri-go <36702009+sri-go@users.noreply.github.com>
2020-10-05 12:54:03 -0400

    remove files from git track

--- react-firebase/todo.js
+++ react-firebase/todo.js
@@ -1,15 +1,10 @@
 /*
 To Do: 
--> fix database security permissions for users
--> fix admin portal 
-
 pre: compress image on add
 1) upload file to storage
 2) write values to database with url for profile image download
 
 when updating email -> make sure to update users/db as well otherwise unlinked updates exist
-
-figure out routing for account page
 
 admin add delete all data
 */
